71f06e2083d7f0859a3fcbbefbacc3156d217c20
fatma2705 <arafatma2003@gmail.com>
2024-05-21 14:28:15 +0200

    interfaccia service per model autore creata

---
+++ src/main/java/it/prova/biblioteca/service/AutoreService.java
@@ -0,0 +1,27 @@
+package it.prova.biblioteca.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+import it.prova.biblioteca.model.Autore;
+
+public interface AutoreService {
+
+	public List<Autore> listAllElements();
+
+	public Autore caricaSingoloElemento(Long id);
+
+	public Autore caricaSingoloElementoConFilms(Long id);
+
+	public void aggiorna(Autore autoreInstance);
+
+	public void inserisciNuovo(Autore autoreInstance);
+
+	public void rimuovi(Long idToDelete);
+
+	public List<Autore> findByExample(Autore example);
+
+	public Page<Autore> findByExampleWithPagination(Autore example, Integer pageNo, Integer pageSize, String sortBy);
+
+}
