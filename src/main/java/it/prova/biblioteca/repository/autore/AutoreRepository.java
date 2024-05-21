ad3b5901efa4ac6397d33132b379909cf571886f
fatma2705 <arafatma2003@gmail.com>
2024-05-21 10:48:11 +0200

    Classi repositori per model Autore modificate e finite

--- src/main/java/it/prova/biblioteca/repository/autore/AutoreRepository.java
+++ src/main/java/it/prova/biblioteca/repository/autore/AutoreRepository.java
@@ -6,7 +6,8 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 
 import it.prova.biblioteca.model.Autore;
 
-public interface AutoreRepository extends PagingAndSortingRepository<Autore, Long>, JpaSpecificationExecutor<Autore> {
+public interface AutoreRepository
+		extends PagingAndSortingRepository<Autore, Long>, JpaSpecificationExecutor<Autore>, CustomAutoreRepository {
 
 	@Query("select a from Autore a left join fetch a.libri where a.id = ?1")
 	public Autore findByidEager(Long id);
--- src/main/java/it/prova/biblioteca/repository/autore/CustomAutoreRepositoryImpl.java
+++ src/main/java/it/prova/biblioteca/repository/autore/CustomAutoreRepositoryImpl.java
@@ -5,26 +5,51 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import io.micrometer.common.util.StringUtils;
+import org.apache.commons.lang3.StringUtils;
+
 import it.prova.biblioteca.model.Autore;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 
 public class CustomAutoreRepositoryImpl implements CustomAutoreRepository {
-	
+
 	@PersistenceContext
 	private EntityManager entityManager;
 
-//	@Override
-//	public List<Autore> findByExample(Autore example) {
-//		Map<String,Object> paramaterMap = new HashMap<String,Object>();
-//		List<String> whereClauses= new ArrayList<String>();
-//		
-//		StringBuilder queryBuilder = new StringBuilder("select a from Autore a where a.id=a.id");
-//		
-//		if (StringUtils.isNotEmpty(example.getNome())) {
-//			
-//		}
-//	}
+	@Override
+	public List<Autore> findByExample(Autore example) {
+		Map<String, Object> paramaterMap = new HashMap<String, Object>();
+		List<String> whereClauses = new ArrayList<String>();
+
+		StringBuilder queryBuilder = new StringBuilder("select a from Autore a where a.id=a.id");
+
+		if (StringUtils.isNotEmpty(example.getNome())) {
+			whereClauses.add(" a.nome like :nome");
+			paramaterMap.put("nome", "%" + example.getNome() + "%");
+		}
+		if (StringUtils.isNotEmpty(example.getCognome())) {
+			whereClauses.add(" a.cognome like :cognome ");
+			paramaterMap.put("cognome", "%" + example.getCognome() + "%");
+		}
+		if (StringUtils.isNotEmpty(example.getNomeArte())) {
+			whereClauses.add(" a.nomeArte like :nomeArte");
+			paramaterMap.put("nomeArte", "%" + example.getNomeArte() + "%");
+		}
+		if (example.getDataNascita() != null) {
+			whereClauses.add("a.dataNascita >= :dataNascita ");
+			paramaterMap.put("dataNascita", example.getDataNascita());
+		}
+
+		queryBuilder.append(!whereClauses.isEmpty() ? " and" : "");
+		queryBuilder.append(StringUtils.join(whereClauses, " and "));
+		TypedQuery<Autore> typedQuery = entityManager.createQuery(queryBuilder.toString(), Autore.class);
+
+		for (String key : paramaterMap.keySet()) {
+			typedQuery.setParameter(key, paramaterMap.get(key));
+		}
+
+		return typedQuery.getResultList();
+	}
 
 }
